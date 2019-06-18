7b1f78287e1b3a1471fa7110454a55ab660dfec9
Gerasimos Filippakos <gerfil18-github@yahoo.com>
2019-06-18 18:15:25 +0300

    Added Prefabs, ExitInfo script fully functional, exits in prefabs
    +ExitInfo script on each exit, with list of what it can connect and if it is or not connected
    +Prefabs for Room, Junction, and Corridor. 
    ++Room has prefabs with 1, 2, 3 and 4 exits
    ++Junction has prefabs with 3 and 4 exits

---
+++ AI_Project_vol2/Assets/Scripts/ExitInfo.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitInfo : MonoBehaviour
+{ 
+    public enum ModuleType { Room, Corridor, Junction, InvalidType }
+
+    private ModuleType[] _connectableModules;
+    private ModuleType _moduleType;
+
+    private string _parentTag;
+
+    private bool _connected = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _parentTag = transform.parent.tag;
+        _moduleType = ConvertTagToModuleType(_parentTag);
+
+        ////initialize module type
+        //if (_parentTag == "Room")
+        //{
+        //    _moduleType = ModuleType.Room;
+        //}
+        //else if (_parentTag == "Corridor")
+        //{
+        //    _moduleType = ModuleType.Corridor;
+        //}
+        //else if (_parentTag == "Junction")
+        //{
+        //    _moduleType = ModuleType.Junction;
+        //}
+
+        switch (_moduleType)
+        {
+            case ModuleType.Room:
+                _connectableModules = new ModuleType[1]; //rooms can only connect to corridors
+                _connectableModules[0] = ModuleType.Corridor; 
+                break;
+
+            case ModuleType.Corridor:
+                _connectableModules = new ModuleType[2]; //corridors can connect to both rooms and junctions
+                _connectableModules[0] = ModuleType.Room;
+                _connectableModules[1] = ModuleType.Junction; 
+                break;
+
+            case ModuleType.Junction:
+                _connectableModules = new ModuleType[1]; //junctions can only connect to corridors
+                _connectableModules[0] = ModuleType.Corridor;
+                break;
+        }
+    }
+	
+    /// <summary>
+    /// Marks the exit as connected. After this is called,
+    /// CheckIfConnected will always return true.
+    /// </summary>
+	public void MarkAsConnected() { _connected = true; }
+
+    /// <summary>
+    /// Returns the value of _connected. If true, there is no need to connect anything more to this exit.
+    /// If false, this exits needs something to connect to!
+    /// </summary>
+    /// <returns></returns>
+    public bool CheckIfConnected() { return _connected; }
+
+    /// <summary>
+    /// Converts a module's tag to a ModuleType enum. If the string given is invalid,
+    /// returns the "InvalidType" ModuleType enum.
+    /// </summary>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    public ModuleType ConvertTagToModuleType(string tag)
+    {
+        if (tag == "Room")
+        {
+            return ModuleType.Room;
+        }
+        else if (tag == "Corridor")
+        {
+            return ModuleType.Corridor;
+        }
+        else if (tag == "Junction")
+        {
+            return ModuleType.Junction;
+        }
+        else
+        {
+            return ModuleType.InvalidType;
+        }
+    }
+}
