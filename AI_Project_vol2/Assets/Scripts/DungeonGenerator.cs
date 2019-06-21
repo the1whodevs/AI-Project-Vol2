17a91312ea7c882730a9da2b48fa0b3bcdb87138
Gerasimos Filippakos <gerfil18-github@yahoo.com>
2019-06-21 21:28:40 +0300

    Adjusted code, found issue, TODO!

--- AI_Project_vol2/Assets/Scripts/DungeonGenerator.cs
+++ AI_Project_vol2/Assets/Scripts/DungeonGenerator.cs
@@ -10,7 +10,7 @@ public class DungeonGenerator : MonoBehaviour
     [SerializeField] private GameObject[] _junctionPrefabs; //index + 3 = number of exits 
     [SerializeField] private GameObject _corridorPrefab; //exactly 2 exits
 
-    [SerializeField] private int _minIterations; //the algorithm will keep spawning until this number is reached
+    [SerializeField] private int _minModulesToHave; //the algorithm will keep spawning until this number is reached
 
     private GameObject _currentModule; //the current module we're checking to connect all exits
     private ModuleInfo _currentModuleInfo; //the current moduleInfo of the module we're checking to connect all exits
@@ -30,43 +30,16 @@ public class DungeonGenerator : MonoBehaviour
 
     private void GenerateRandomDungeon() //TODO: Make it so this is repeated until enough modules are in the map!
     {
-        SetStartingModule();
-
-        _currentModule = Instantiate(_allModulesToConnect.Dequeue(), Vector3.zero, Quaternion.identity); //Instantiate the module 
-
-        for (int j = 0; j < _minIterations; j++)
-        {
-            _currentModuleInfo = _currentModule.GetComponent<ModuleInfo>();
-
-            int childrenCount = _currentModule.transform.childCount;
-
-            for (int i = 0; i < childrenCount; i++)
-            {
-                GameObject _currentExit = _currentModule.transform.GetChild(i).gameObject;
-                ExitInfo _exitInfoToCheck = _currentExit.GetComponent<ExitInfo>();
-
-                if (!_exitInfoToCheck.CheckIfConnected())
-                {
-                    _exits.Enqueue(_currentExit);
-                }
-            }
-
-            while (_exits.Count > 0) //&& _moduleCount < _minIterations
-            {
-                GameObject _exitToConnectA = _exits.Dequeue(); //dequeues an exit gameobject that also has an exitinfo component that is not marked as connected.
-
-                GameObject _suitableModule = FindSuitableModule(_currentModuleInfo); //select an appropriate module to instantiate
-                //_allModulesToConnect.Enqueue(_suitableModule);
-                GameObject _exitToConnectB = _suitableModule.transform.Find("Exit").gameObject;
-
-                ConnectExits(_exitToConnectA,
-                    _exitToConnectB); //pick an exit from the _suitableModule, and connect it with a !connected exit of the _currentModule 
-            }
-
-            Debug.Log(_moduleCount);
-            Debug.Log(_minIterations);
-        }
-
+        InitializeWithStartingModule();
+        //Debug.Log(_allModulesToConnect.Count);
+        //while (_moduleCount < _minModulesToHave && _allModulesToConnect.Count > 0)
+        //{
+        _currentModule = _allModulesToConnect.Dequeue(); //Dequeue the module, since it's already instantiated and then queued
+        //Debug.Log(_allModulesToConnect.Count);
+        _currentModuleInfo = _currentModule.GetComponent<ModuleInfo>(); //Get module info
+        EnqueueExits(_currentModule); //Enqueue all exits 
+        //ConnectAllExits(); //Connect all exits 
+        //}
     }
 
     /// <summary>
@@ -77,11 +50,11 @@ public class DungeonGenerator : MonoBehaviour
     /// <param name="exitA"></param>
     /// <param name="exitB"></param>
     /// <returns></returns>
-    private void ConnectExits(GameObject exitA, GameObject exitB)
+    private void ConnectExits(GameObject exitA, GameObject exitB) //TODO: Fix rotating num (currently 90) so we dont get stuck in an infinite loop!
     {
         //Match the positions of the exits, have their Transform.forward opposite and their Transform.up the same.
         while (exitA.transform.forward != exitB.transform.forward * -1)
-        {
+        { 
             exitB.transform.parent.Rotate(90 * Vector3.up);
         }
 
@@ -136,19 +109,16 @@ public class DungeonGenerator : MonoBehaviour
             case ModuleType.Room:
                 exitRand = Random.Range(0, _roomPrefabs.Length);
                 gameObjectToReturn = Instantiate(_roomPrefabs[exitRand]);
-                _moduleCount++;
                 _allModulesToConnect.Enqueue(gameObjectToReturn);
                 return gameObjectToReturn;
             case ModuleType.Junction:
                 exitRand = Random.Range(0, _junctionPrefabs.Length); // remember that index + 3 = number of exits!
                 gameObjectToReturn = Instantiate(_junctionPrefabs[exitRand]);
                 _allModulesToConnect.Enqueue(gameObjectToReturn);
-                _moduleCount++;
                 return gameObjectToReturn;
             case ModuleType.Corridor:
                 gameObjectToReturn = Instantiate(_corridorPrefab);
                 _allModulesToConnect.Enqueue(gameObjectToReturn);
-                _moduleCount++;
                 return gameObjectToReturn;
         }
 
@@ -156,31 +126,66 @@ public class DungeonGenerator : MonoBehaviour
         return gameObjectToReturn = new GameObject("I should never exist.");
     }
 
-    private void SetStartingModule()
+    private void InitializeWithStartingModule()
     {
-        int typeRandom = -1; //0 is room, 1 is junction, 2 is corridor 
-
-        typeRandom = Random.Range(0, 2); //no corridors for startingModule!
-
+        int typeRandom = Random.Range(0, 2); //no corridors for startingModule! 0 is room, 1 is junction
         int exitsRandom = -1; //if this is -1 after the typeRandom if statements, something went wrong!
-
-        GameObject _startingModule = new GameObject("Starting Module");
 
         if (typeRandom == 0)
         {
             //room
-            exitsRandom = Random.Range(0, _roomPrefabs.Length); // remember that index + 1 = number of exits!
-            _startingModule = _roomPrefabs[exitsRandom];
+            exitsRandom = Random.Range(1, _roomPrefabs.Length); // remember that index + 1 = number of exits!
+            _currentModule = _roomPrefabs[exitsRandom];
         }
         else if (typeRandom == 1)
         {
             //junction
             exitsRandom = Random.Range(0, _junctionPrefabs.Length); // remember that index + 3 = number of exits!
-            _startingModule = _junctionPrefabs[exitsRandom];
+            _currentModule = _junctionPrefabs[exitsRandom];
+        }
+        else
+        {
+            _currentModule = new GameObject("I shouldn't exist?");
         }
 
         _moduleCount++;
-        _allModulesToConnect.Enqueue(_startingModule);
-        //return Instantiate(_startingModule, Vector3.zero, Quaternion.identity); //Instantiate the module
+        _currentModule = Instantiate(_currentModule, Vector3.zero, Quaternion.identity); //Instantiate the module 
+        _currentModuleInfo = _currentModule.GetComponent<ModuleInfo>(); //Get module info
+        EnqueueExits(_currentModule); //Enqueue all exits 
+        ConnectAllExits(); //Connect all exits
+    }
+
+    private void ConnectAllExits()
+    {
+        while (_exits.Count > 0)
+        {
+            GameObject _exitToConnectA = _exits.Dequeue(); //dequeues an exit gameobject that also has an exitinfo component that is not marked as connected.
+            Debug.Log("Exits: " + _exits.Count);
+
+            GameObject _suitableModule = FindSuitableModule(_currentModuleInfo); //select an appropriate module to instantiate
+            _moduleCount++; //we're increasing the module count since FindSuitableModule spawns a module!
+
+            GameObject _exitToConnectB = _suitableModule.transform.Find("Exit").gameObject;
+
+            ConnectExits(_exitToConnectA,
+                _exitToConnectB); //pick an exit from the _suitableModule, and connect it with a !connected exit of the _currentModule 
+        }
+    }
+
+    private void EnqueueExits(GameObject parentModule)
+    {
+        int childrenCount = parentModule.transform.childCount;
+
+        for (int i = 0; i < childrenCount; i++)
+        {
+            GameObject _currentExit = parentModule.transform.GetChild(i).gameObject;
+            ExitInfo _exitInfoToCheck = _currentExit.GetComponent<ExitInfo>();
+
+            if (!_exitInfoToCheck.CheckIfConnected())
+            {
+                _exits.Enqueue(_currentExit);
+                Debug.Log("Exits: " + _exits.Count);
+            }
+        }
     }
 }
